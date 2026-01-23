32702abe25c12d6f61369f877498ce5cc2182f5d
yerayduran <yerayduranchaves2006@gmail.com>
2026-01-23 14:06:23 +0100

    Estructura inicial del Test VideojuegoServiceTest

---
+++ src/test/java/VideojuegoServiceTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VideojuegoServiceTest {
+
+    @Test
+    void registrarJuego() {
+    }
+
+    @Test
+    void clasificarJuego() {
+    }
+
+    @Test
+    void esJuegoLargo() {
+    }
+
+    @Test
+    void calcularTotalHoras() {
+    }
+}
